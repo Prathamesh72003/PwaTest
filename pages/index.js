1ab60daf1c191448a71be3e515c62d3eeee98a0d
Prathamesh72003 <pthakare72003@gmail.com>
2024-01-08 20:42:00 +0530

    spel mistakes

--- pages/index.js
+++ pages/index.js
@@ -109,7 +109,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            stantly deploy your Next.js site to a shareable URLl.
           </p>
         </a>
       </div>
